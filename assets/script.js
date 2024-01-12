8c82545f461e0d4e6bf41336b444b337dc1e19a0
RowanKinross <rowankinross@yahoo.co.uk>
2024-01-12 19:25:21 +0000

    todays_weather_displays_fully_with_icon_and_text_data

--- assets/script.js
+++ assets/script.js
@@ -19,7 +19,6 @@ $(`#search-form`).on(`submit`, function(e){
 searchHistory.on(`click`, function(e){
   e.preventDefault()
   city = e.target.innerHTML
-  console.log(city)
   loadWeatherInfo()
 })
 
@@ -28,16 +27,21 @@ searchHistory.on(`click`, function(e){
 
 
 function loadWeatherInfo() {
+  
+  
   //to find lat/lon from the city name:
   const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`;
-
-
-// fetch call
-fetch(queryURL)
+  
+  
+  // fetch call
+  fetch(queryURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
+
+
+
 
     //for today's weather url =
     const weatherQueryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=${APIKey}`
@@ -47,19 +51,22 @@ fetch(queryURL)
       return response.json()
     })
     .then(function (data) {
-      // The city name
-      const cityName = $(`<p>`).text(city).addClass(`cityName`)
-      // The date
-      // An icon representation of weather conditions
-      // The temperature
-      // The humidity
-      // The wind speed
-      //all the fetch data in an object
-      console.log(data);
-      today.append(cityName)
-
+      //first clear old today entries
+      today.empty()
       
+      // New today's data entries:
+      const date = dayjs().format('DD/MM/YYYY') // The date
+      const cityNameDate = $(`<h3>`).text(`${city} (today: ${date}) - ${data.weather[0].description}`).addClass(`cityName `) // City Name
+      const weatherIcon = $(`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" id="image" alt="" class="icon" />`) // Weather conditions Icon
+      const temp = Math.round(10*(data.main.temp))/10 // The temperature
+      const feelsLike = Math.round(data.main.feels_like) // Feels-like temperature
       
+      const tempEl = $(`<p>`).text(`Temperature: ${temp}°C (Feels like ${feelsLike}°C)`)
+      const humidityEl = $(`<p>`).text(`Humidity: ${data.main.humidity}%`) // Humidity
+      const windEl = $(`<p>`).text(`Wind Speed: ${data.wind.speed}m/s`) // Wind speed
+      
+      //all the fetch data printed to html:
+      today.append(cityNameDate, weatherIcon, tempEl, humidityEl, windEl)
     }) // <-- end of today's weather fetch function
 
 
@@ -76,8 +83,7 @@ fetch(queryURL)
     })
     .then(function (data) {
       //all the fetch data in an object
-     console.log(data);
-
+     
 
 
 
@@ -88,15 +94,16 @@ fetch(queryURL)
 
 
 //! Create a weather dashboard with form inputs.
-// When a user searches for a city they are presented with current and future conditions for that city and 
-    //that city is added to the search history
-// When a user views the current weather conditions for that city they are presented with:
-// The city name
-// The date
-// An icon representation of weather conditions
-// The temperature
-// The humidity
-// The wind speed
+//! When a user searches for a city they are presented with current and future conditions for that city and 
+    //!that city is added to the search history
+//! When a user views the current weather conditions for that city they are presented with:
+//! The city name
+//! The date
+//! An icon representation of weather conditions
+//! The temperature
+//! The humidity
+//! The wind speed
+
 // When a user view future weather conditions for that city they are presented with a 5-day forecast that displays:
 // The date
 // An icon representation of weather conditions
