e394daf7a986987f2f44f6d182748816bac40f59
RowanKinross <rowankinross@yahoo.co.uk>
2024-01-12 14:13:39 +0000

    buttons_added_for_search_history

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,28 @@
+const APIKey = apiKey //insert own API key here
+const searchHistory = $(`#history`)
+var city = ``;
+
 // submit event when a user searches for a city
 $(`#search-form`).on(`submit`, function(e){
-  e.preventDefault();
-
-  const city = $(`#search-input`).val()
-
+  e.preventDefault()
+  city = $(`#search-input`).val()
   if (city != ``){
+  const cityButton = $(`<button>`).text(city).addClass(`btn`).attr(`city-name`, city)
+  searchHistory.prepend(cityButton);
+  loadWeatherInfo()
+  }
+}) // <-- end of search form submit
 
-  const APIKey = apiKey
+//click event when a user clicks on search history
+searchHistory.on(`click`, function(e){
+  e.preventDefault()
+  city = e.target.innerHTML
+  console.log(city)
+  loadWeatherInfo()
+})
+
+function loadWeatherInfo() {
+  //to find lat/lon from the city name:
   const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`;
 
 
@@ -16,32 +32,57 @@ fetch(queryURL)
     return response.json();
   })
   .then(function (data) {
-    //new url with lat and lon taken from the original url, transposed into the new url + metric units added
-    const newQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&cnt=5&units=metric&appid=${APIKey}`
+
+    //for today's weather url =
+    const weatherQueryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=${APIKey}`
+    //fetch today's weather with new url
+    fetch(weatherQueryUrl)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      // The city name
+
+      // The date
+      // An icon representation of weather conditions
+      // The temperature
+      // The humidity
+      // The wind speed
+      //all the fetch data in an object
+      console.log(data);
+
+      
+      
+    }) // <-- end of today's weather fetch function
+
+
+
+
+    //for 5 day forecast url =
+    const forecastQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&cnt=5&units=metric&appid=${APIKey}`
 
     
-    //new fetch with new url
-    fetch(newQueryUrl)
+    //fetch 5 day forecast with new url
+    fetch(forecastQueryUrl)
     .then(function (response) {
       return response.json()
     })
     .then(function (data) {
       //all the fetch data in an object
-      console.log(data);
+     console.log(data);
 
-      
-      // the temperature (in degrees C)
-      //const tempC = data.main.temp
-      //$('.temp').text(`${tempC}C`)
 
-    }) // <-- end of second fetch function
-  }) // <-- end of first fetch function
-  } else {}// <-- end of city not undefined function
-}) // <-- end of search form submit
+
+
+
+    }) // <-- end of 5 day forecast fetch function
+  }) // <-- end of find lat & lon fetch function
+} // <--end of loadWeatherInfo function
 
 
 //! Create a weather dashboard with form inputs.
-// When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history
+// When a user searches for a city they are presented with current and future conditions for that city and 
+    //that city is added to the search history
 // When a user views the current weather conditions for that city they are presented with:
 // The city name
 // The date
