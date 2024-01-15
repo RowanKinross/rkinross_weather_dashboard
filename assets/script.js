42a1bd063a299510d04fda6ecce79ecec430633a
RowanKinross <rowankinross@yahoo.co.uk>
2024-01-15 12:12:07 +0000

    comments_added_to_html_script_and_CSS

--- assets/script.js
+++ assets/script.js
@@ -5,24 +5,31 @@ var cityArr = []
 const today = $(`#today`);
 const forecast = $(`#forecast`)
 
+
+
+// first get any locally stored search history
 if (Object.entries(localStorage) != ``){
 const storedCities = localStorage.getItem(`cityArr`).split(`,`)
-console.log(storedCities)
 searchHistory.empty()
 if (storedCities != null){
   for (let i=0; i<storedCities.length; i++){
   city = storedCities[i]
   prependCityButton()
+  cityArr.push(city)
   }
 loadWeatherInfo()
 }}
 
 
+
+
+//prepend searched for cities as buttons in a function:
 function prependCityButton(){
     if (city != ``){
     const cityButton = $(`<button>`).text(city).addClass(`btn btn-outline-primary m-1`).attr(`city-name`, city)
     searchHistory.prepend(cityButton);
   }}
+
 
 
 
@@ -33,15 +40,22 @@ $(`#search-form`).on(`submit`, function(e){
   if (city != ``){
     // load the weather info for that city
     loadWeatherInfo()
+    //if the cityArr includes the city already, remove it and push it again(to the start)
+    if (cityArr.includes(city)){
+    const index = cityArr.indexOf(city)
+    cityArr.splice(index, 1);}
+    else{
     // add the city as a button in the search history
     prependCityButton()
+    }
     // push city to cityArr
-    // cityArr.push(city);
     cityArr.push(city)
     // set the array in local storage
     localStorage.setItem(`cityArr`, cityArr)
   }
-})
+  }
+)
+
 
 
 
@@ -55,14 +69,14 @@ searchHistory.on(`click`, function(e){
 
 
 
-
+// load weather information function: today's & 5 day forecast
 function loadWeatherInfo() {
   if (city != ``){
   //to find lat/lon from the city name:
   const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`;
   
   
-  // fetch call
+  // fetch call to find lat & lon for another queryURL
   fetch(queryURL)
   .then(function (response) {
     return response.json();
@@ -81,12 +95,13 @@ function loadWeatherInfo() {
       today.empty()
       
 
-      // New today's data entries:
+      // Today's data entries:
       const date = dayjs().format('DD/MM/YYYY') // The date
       const cityNameDate = $(`<h3>`).text(`${city} (today: ${date}) - ${data.weather[0].description}`).addClass(`cityName `) // City Name
       const weatherIcon = $(`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" id="image" alt="" class="icon" />`) // Weather conditions Icon
       const temp = Math.round(10*(data.main.temp))/10 // The temperature
       const feelsLike = Math.round(data.main.feels_like) // Feels-like temperature
+
       
       const tempEl = $(`<p>`).text(`Temperature: ${temp}°C (Feels like ${feelsLike}°C)`)
       const humidityEl = $(`<p>`).text(`Humidity: ${data.main.humidity}%`) // Humidity
@@ -109,18 +124,19 @@ function loadWeatherInfo() {
       return response.json()
     })
     .then(function (data) {
-      // console.log(data)
-      //all the fetch data in an object
+    //clear exiting html forecast data
     forecast.empty()
-
     // The date
     const a = dayjs()
 
+    //make a card for each day in the forecast (based on bootstrap cards)
     for (let i=1; i<6; i++ ){
       //date = dayjs() plus 1 day for each card
       const forecastDate = a.add(i, 'day').format('DD/MM/YYYY')
       //arr for each forecast day card
       const forecastDayArr = data.list[i*8-1]
+
+      //forcast data variables:
       const foreTemp = $(`<p>`).text(`Temp: ${Math.round(forecastDayArr.main.temp)}°C`) 
       const foreHumidity = $(`<p>`).text(`Humidity: ${forecastDayArr.main.humidity}%`)
       const foreWind = $(`<p>`).text(`Wind: ${forecastDayArr.wind.speed}m/s`)
@@ -145,9 +161,3 @@ function loadWeatherInfo() {
   //Github Secrets for API key
   //Deploy & Screenshot/gif
   //submit
-  //error code on opening page if no cities are in array
-
-//Optional:
-  //Styling
-  //limit search history length
-  //clear search history
